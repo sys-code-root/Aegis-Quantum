6ed54ad22da1077a8b4b2fb6a3d37d8db5acc740
sys-code-root <directcontact2027@gmail.com>
2026-06-24 18:15:31 -0300

    Refactor QuantumNeuralNetworkSetup class and methods

--- Quantum_Security/052_quantum_neural_network_setup/neural_setup.py
+++ Quantum_Security/052_quantum_neural_network_setup/neural_setup.py
@@ -3,36 +3,26 @@ from qiskit.circuit import ParameterVector
 from qiskit_machine_learning.neural_networks import EstimatorQNN
 
 class QuantumNeuralNetworkSetup:
-    """
-    Initializes a formal Quantum Neural Network (QNN) using the Qiskit 
-    Estimator primitive for scalable quantum machine learning.
-    """
     def __init__(self, n_inputs: int = 2, n_weights: int = 2):
         self.n_inputs = n_inputs
         self.n_weights = n_weights
-
-        # ParameterVectors act as scalable arrays for inputs and trainable weights
         self.inputs = ParameterVector('input', n_inputs)
         self.weights = ParameterVector('weight', n_weights)
         self.qc = self._build_circuit()
 
     def _build_circuit(self) -> QuantumCircuit:
-        """Constructs the variational quantum circuit layers."""
         qc = QuantumCircuit(2)
 
-        # 1. Feature Map Layer: Encoding input data into quantum states
         qc.ry(self.inputs[0], 0)
         qc.ry(self.inputs[1], 1)
 
-        # 2. Ansatz Layer: Learnable rotation weights and entanglement
         qc.rx(self.weights[0], 0)
         qc.rx(self.weights[1], 1)
-        qc.cx(0, 1)  # Entangling gate to capture feature correlations
+        qc.cx(0, 1)
 
         return qc
 
     def get_qnn(self) -> EstimatorQNN:
-        """Wraps the circuit into a trainabale QNN object."""
         return EstimatorQNN(
             circuit=self.qc,
             input_params=self.inputs,
